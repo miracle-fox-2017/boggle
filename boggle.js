efa66a94c0f69420a6a2ac056532b01ae04547e4
vbagustinus <vbagustinus@gmail.com>
2017-10-22 04:17:50 +0700

    Release 1

--- boggle.js
+++ boggle.js
@@ -1,8 +1,9 @@
 class Boggle {
   constructor(file) {
-    this.kamus = file
-    this.papan = []
-    this.hasil = []
+    this.kamus = file;
+    this.papan = [];
+    this.hasil = [];
+    this.hint =  true;
   }
   randomAphabet(){
     let huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -19,27 +20,40 @@ class Boggle {
     return this.papan;
   }
   solver(){
-    for (var i = 0; i < this.kamus.length; i++) {
-      for (var j = 0; j < this.kamus[i].length; j++) {
-        console.log(this.kamus[i][j]);
+    let listAround = [];
+    let start = 0;
+    this.kamus.forEach(dataKamus =>{
+      for (var x = 0; x < this.papan.length; x++) {
+        let dataPapan = this.papan[x]
+        for (var y = 0; y < dataPapan.length; y++) {
+          if(dataPapan[y].indexOf(dataKamus[start]) !== -1){
+            listAround.push(this.chekAround(x,y))
+          }
+        }
       }
-    }
-    return this.kamus
+    })
+    return coba
   }
   chekAround(posX, posY){
-      console.log('kunci',this.shake[posX][posX])
-      let cek=[]
-        for (let startX = -1; startX <= 1; startX++){
-            for (let startY = -1; startY <= 1; startY++){
-                let idxX = posX + startX;
-                let idxY = posY + startY;
-                cek.push(data[idxX][idxY])
+    // console.log('key'+this.papan[posX][posX]);
+    let cek = []
+      for (let startX = -1; startX <= 1; startX++){
+        for (let startY = -1; startY <= 1; startY++){
+          let idxX = posX + startX;
+          let idxY = posY + startY;
+          if(typeof this.papan[idxX] !== 'undefined'){
+            if(typeof this.papan[idxX][idxY] !== 'undefined'){
+              cek.push(this.papan[idxX][idxY])
             }
+          }
         }
+      }
     return cek
   }
 }
 var arr = require('./coba.js');
 let boggle = new Boggle(arr)
-console.log(boggle.shake(8));
+console.log(boggle.shake(4));
+// console.log(boggle.papan);
 console.log(boggle.solver());
+// console.log(boggle.chekAround(0,0));
