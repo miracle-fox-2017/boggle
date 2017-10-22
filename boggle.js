0e9eb97cda8bec16bd533616a1897d363febc809
aditReza <lete.note3@gmail.com>
2017-10-22 16:36:01 +0700

    otw relese 1

--- boggle.js
+++ boggle.js
@@ -1,17 +1,58 @@
 const fs = require('fs')
 const newArr = fs.readFileSync('./data.js')
-// console.log(newArr.length);
+// console.log('import >>>', newArr.length);
 
 class BoggleBoard{
   constructor(dictionary){
     this.dictionary = dictionary
+    this.dummyData  = ['data','array','nested','ball','basket']
     this.dummy      = []
     this.onBoard    = []
+
+  }
+
+  solve(row, col){
+    // console.log('-------', this.onBoard);
+
+    let result = []
+    // console.log('key >>',this.onBoard[row][col]);
+
+    for(let r=0; r<3; r++){
+      for(let c=0; c<3; c++){
+          if (typeof this.onBoard[r+(row-1)] !== 'undefined') {
+            if(typeof this.onBoard[c+(col-1)] !== 'undefined'){
+              result.push(this.onBoard[r+(row-1)][c+(col-1)])
+            }
+          }
+      }
+    }
+    // console.log(result);
+    return result
+  }
+
+  checkData(){
+    // console.log('load data >>', this.dummyData[0][0])
+    console.log('board >>>>>>>>', this.onBoard);
+    let frontArr = []
+    for(let i=0; i<this.dummyData.length; i++){
+      frontArr.push(this.dummyData[i][0])
+    }
+
+    console.log('huruf depan >>>>>',frontArr)
+    for(let j=0; j<this.onBoard.length; j++){
+      for(let k=0; k<frontArr.length; k++){
+        console.log('found >>>>', this.onBoard[j].indexOf(frontArr[k]));
+        // if(this.onBoard[j].indexOf(frontArr[k]) !== -1){
+        //   return true
+        // }
+      }
+    }
+    // return false
   }
 
   makeRandom(shake) {
     let squareArea     = shake * shake
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const possible = 'abcdefghijklmnopqrstuvwxyz'
 
     for (var i=0; i<squareArea; i++){
       this.dummy.push(possible.charAt(Math.floor(Math.random() * possible.length)))
@@ -30,11 +71,17 @@ class BoggleBoard{
   }
 
 
+
+
 }
 
 let boggle = new BoggleBoard(newArr)
 
 
 // boggle.makeRandom()
-// boggle.shake()
-console.log(boggle.shake(4))
+boggle.shake(4)
+
+
+// console.log(boggle.shake(4))
+// console.log(boggle.solve(0,0))
+console.log(boggle.checkData())
