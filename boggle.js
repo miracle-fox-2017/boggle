271cd7443cf9f3f6ac51b8291ba9e6dd2197f4ea
Ayunda <ayundahamzah@gmail.com>
2017-10-22 13:32:05 +0700

    release 0

---
+++ boggle.js
@@ -0,0 +1,26 @@
+class Boggle{
+  constructor(words){
+    this.letter='abcdefghijklmnopqrstuvwxyz';
+    this.words=words;
+    this.board=[];
+    this.hasil=0
+  }
+//menghasilkan board sebesar paramxparam
+// untuk mengeluarkan huruf random dari object letter
+  shake(length){
+    // this.letterRandom()
+    // let counter=0;
+    for(let i=0;i<length;i++){
+      this.board.push([])
+      for(let j=0;j<length;j++){
+        this.hasil = Math.floor(Math.random()*26);
+        this.board[i].push(this.letter[this.hasil])
+        // counter++
+      }
+    }
+    return this.board
+  }
+}
+
+var game = new Boggle ();
+console.log(game.shake(4))
